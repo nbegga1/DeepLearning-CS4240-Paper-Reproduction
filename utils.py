f0193ac40bee77c53034cebeafb73fb973e609f0
nbegga1 <64038272+nbegga1@users.noreply.github.com>
2020-04-20 23:45:05 +0200

    Add files via upload

--- utils.py
+++ utils.py
@@ -47,7 +47,34 @@ def get_files(folder, name_filter=None, extension_filter=None):
 
     return filtered_files
 
+# def rgb_to_onehot(rgb_image, colormap):
+#     '''Function to one hot encode RGB mask labels
+#         Inputs: 
+#             rgb_image - image matrix (eg. 256 x 256 x 3 dimension numpy ndarray)
+#             colormap - dictionary of color to label id
+#         Output: One hot encoded image of dimensions (height x width x num_classes) where num_classes = len(colormap)
+#     '''
+#     num_classes = len(colormap)
+#     shape = rgb_image.shape[:2]+(num_classes,)
+#     encoded_image = np.zeros( shape, dtype=np.int8 )
+#     print(rgb_image.shape)
+#     for i, cls in enumerate(colormap):
+#       encoded_image[:,:,i] = np.all(rgb_image.reshape( (-1,3) ) == colormap[i], axis=1).reshape(shape[:2])
+#     return encoded_image
 
+# def onehot_to_rgb(onehot, colormap):
+#     '''Function to decode encoded mask labels
+#         Inputs: 
+#             onehot - one hot encoded image matrix (height x width x num_classes)
+#             colormap - dictionary of color to label id
+#         Output: Decoded RGB image (height x width x 3) 
+#     '''
+#     single_layer = np.argmax(onehot, axis=-1)
+#     output = np.zeros( onehot.shape[:2]+(3,) )
+#     for k in colormap.keys():
+#         output[single_layer==k] = colormap[k]
+#     return np.uint8(output)
+    
 def pil_loader(data_path, label_path):
     """Loads a sample and label image given their path as PIL images.
     Keyword arguments:
